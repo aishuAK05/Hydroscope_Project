2024dcf3c4b997538d7bf07cbc604f5d92075e05
aishuAK05 <144595305+aishuAK05@users.noreply.github.com>
2025-08-12 15:14:33 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    './templates/**/*.html',
+    './app1/templates/**/*.html',
+    './static/src/css/tailwind.css',
+    // Add other paths if needed
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
